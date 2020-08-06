c9849caec17992418b7f68cc488f8232089c5a99
ilkin Nazarli <ilkinnazarli@gmail.com>
2020-08-06 14:57:16 +0400

    management crud is done

--- Library_App/Windows/ManagementWindow.xaml.cs
+++ Library_App/Windows/ManagementWindow.xaml.cs
@@ -2,6 +2,7 @@
 using Library_App.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,11 +18,13 @@ namespace Library_App.Windows
     public partial class ManagementWindow : Window
     {
         private readonly LibraryContext _context;
-        
+        private Management _management;
+        private Management _selectedManagement;
         public ManagementWindow()
         {
             InitializeComponent();
             _context = new LibraryContext();
+            _management = new Management();
         }
 
         private void BtnAdd_Click(object sender, RoutedEventArgs e)
@@ -34,12 +37,16 @@ namespace Library_App.Windows
             }
 
             if (string.IsNullOrEmpty(TxtName.Text) && string.IsNullOrEmpty(TxtSurname.Text)
-    && string.IsNullOrEmpty(TxtUsername.Text) && string.IsNullOrEmpty(TxtPassword.Text))
+    && string.IsNullOrEmpty(TxtUsername.Text) && string.IsNullOrEmpty(TxtPassword.Text)&& string.IsNullOrEmpty((string)CbxPosition.SelectedItem))
             {
                 MessageBox.Show("bos ola bilmez");
                 return;
             }
-            Management management = new Management
+            //if ()
+            //{
+
+            //}
+            _management = new Management
             {
                 Name = TxtName.Text,
                 Surname = TxtSurname.Text,
@@ -47,10 +54,43 @@ namespace Library_App.Windows
                 Parol = TxtPassword.Text,
                 Position = (Positions)CbxPosition.SelectedItem
             };
-            _context.Managements.Add(management);
+            _context.Managements.Add(_management);
+
             _context.SaveChanges();
-            MessageBox.Show($"{TxtUsername.Text} adli istifadəçi {(Positions)CbxPosition.SelectedItem} əlavə edildi");
+            MessageBox.Show($"{TxtUsername.Text} adli istifadəçi {(Positions)CbxPosition.SelectedItem} statusu ilə əlavə edildi");
             Reset();
+            FillManagements();
+        }
+
+        private void FillManagements()
+        {
+            DgPerson.ItemsSource = _context.Managements.ToList();
+        }
+        private void DgPerson_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (DgPerson.SelectedItem == null) return;
+
+            _selectedManagement = (Management)DgPerson.SelectedItem;
+
+            TxtName.Text = _selectedManagement.Name;
+            TxtSurname.Text = _selectedManagement.Surname;
+            TxtUsername.Text= _selectedManagement.UserName;
+            TxtPassword.Text = _selectedManagement.Parol;
+            CbxPosition.SelectedItem = _selectedManagement.Position;
+        }
+
+        private void BtnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            _selectedManagement.Name = TxtName.Text;
+            _selectedManagement.Surname = TxtSurname.Text;
+            _selectedManagement.UserName = TxtUsername.Text;
+            _selectedManagement.Parol = TxtPassword.Text;
+            _selectedManagement.Position = (Positions)CbxPosition.SelectedItem;
+
+            _context.SaveChanges();
+            Reset();
+            MessageBox.Show("İdarəçi dəyişiklikləri yeniləndi");
+            FillManagements();
         }
 
         private void Reset()
@@ -60,6 +100,26 @@ namespace Library_App.Windows
             TxtUsername.Clear();
             TxtPassword.Clear();
             CbxPosition.SelectedItem = null;
+        }
+
+        private void BtnRead_Click(object sender, RoutedEventArgs e)
+        {
+            FillManagements();
+        }
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult r = MessageBox.Show("Silməyə əminsiniz?", _selectedManagement.ToString(), MessageBoxButton.OKCancel);
+
+            if (r == MessageBoxResult.OK)
+            {
+                _context.Managements.Remove(_selectedManagement);
+                _context.SaveChanges();
+
+                Reset();
+
+                MessageBox.Show($"{_selectedManagement.UserName } adlı İdarəçi silindi") ;
+                FillManagements();
+            }
         }
     }
 }
