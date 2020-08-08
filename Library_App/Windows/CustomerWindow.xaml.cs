8c2ac9cd8787faaba65ac99670512a6083f3aa5e
ilkin Nazarli <ilkinnazarli@gmail.com>
2020-08-08 11:06:12 +0400

    Customer CRUD done with phone number chek validation

---
+++ Library_App/Windows/CustomerWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using Library_App.Data;
+using Library_App.Models;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Library_App.Windows
+{
+    public partial class CustomerWindow :  Window 
+    {
+        private readonly LibraryContext _context;
+        private Customer _customer;
+        private Customer _selectedCustomer;
+        
+        public CustomerWindow()
+        {
+            InitializeComponent();
+            _context = new LibraryContext();
+            _customer = new Customer();
+            
+        }
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Double Number;
+            if (Double.TryParse(TxtName.Text, out Number) && Double.TryParse(TxtSurname.Text, out Number))
+            {
+                MessageBox.Show("Ad ve Soyad reqem ola bilmez");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(TxtName.Text) || string.IsNullOrWhiteSpace(TxtSurname.Text)
+            || string.IsNullOrWhiteSpace(TxtPhone.Text) || string.IsNullOrWhiteSpace(TxtEmail.Text) || string.IsNullOrWhiteSpace(TxtIdCard.Text))
+            {
+                MessageBox.Show("xanalar boş qala bilmez");
+                return;
+            }
+            if ((_context.Customers.Any(r => r.Email == TxtEmail.Text)) || (_context.Customers.Any(r => r.IdCard == TxtIdCard.Text)))
+            {
+                MessageBox.Show($"Bu Username mövcüddür");
+                return;
+            }
+            string[] words = TxtPhone.Text.Split('+', ' ');
+            string PhoneStr = string.Join("", words);
+            if (!Double.TryParse(PhoneStr, out Number))
+            {
+                MessageBox.Show("Telefon nömrəsi hərifdən ibarət ola bilmez");
+                return;
+            }
+            _customer = new Customer
+            {
+                Name = TxtName.Text,
+                Surname = TxtSurname.Text,
+                Phone = "+"+ PhoneStr,
+                Email = TxtEmail.Text,
+                IdCard = TxtIdCard.Text
+            };
+            _context.Customers.Add(_customer);
+            _context.SaveChanges();
+            MessageBox.Show($"{ TxtName.Text} adli müştəri əlavə edildi");
+            Reset(); 
+            FillCustomers();
+
+        }
+        private void DgPerson_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (DgPerson.SelectedItem == null) return;
+
+            _selectedCustomer = (Customer)DgPerson.SelectedItem;
+
+            TxtName.Text = _selectedCustomer.Name;
+            TxtSurname.Text = _selectedCustomer.Surname;
+            TxtPhone.Text = _selectedCustomer.Phone;
+            TxtEmail.Text = _selectedCustomer.Email;
+            TxtIdCard.Text = _selectedCustomer.IdCard;
+            BtnUpdate.Visibility = Visibility.Visible;
+            BtnDelete.Visibility = Visibility.Visible;
+        }
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult r = MessageBox.Show("Silməyə əminsiniz?", _selectedCustomer.ToString(), MessageBoxButton.OKCancel);
+            if (r == MessageBoxResult.OK)
+            {
+                _context.Customers.Remove(_selectedCustomer);
+                _context.SaveChanges();
+                Reset();
+                MessageBox.Show($"{_selectedCustomer.Name}adlı istifadəçi silindi");
+                FillCustomers();
+            }
+        }
+        private void BtnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            Double Number;
+            string[] words = TxtPhone.Text.Split('+', ' ');
+            string PhoneStr = string.Join("", words);
+            if (!Double.TryParse(PhoneStr, out Number))
+            {
+                MessageBox.Show("Telefon nömrəsi hərifdən ibarət ola bilmez");
+                return;
+            }
+            _selectedCustomer.Name = TxtName.Text;
+            _selectedCustomer.Surname = TxtSurname.Text;
+            _selectedCustomer.Phone = "+" + PhoneStr;
+            _selectedCustomer.Email = TxtEmail.Text;
+            _selectedCustomer.IdCard = TxtIdCard.Text;
+            _context.SaveChanges();
+            Reset();
+            MessageBox.Show("Müştəri məlumatları yeniləndi");
+            FillCustomers();
+        }
+        private void BtnRead_Click(object sender, RoutedEventArgs e)
+        {
+            FillCustomers();
+            Reset();
+        }
+        private void FillCustomers()
+        {
+            DgPerson.ItemsSource = _context.Customers.ToList();
+
+        }
+        private void Reset()
+        {
+            TxtName.Clear();
+            TxtSurname.Clear();
+            TxtPhone.Clear();
+            TxtEmail.Clear();
+            TxtIdCard.Clear();
+            BtnUpdate.Visibility = Visibility.Hidden;
+            BtnDelete.Visibility = Visibility.Hidden;
+            BtnRead.Visibility = Visibility.Hidden;
+        }
+
+    }
+}
--- Library_App/Windows/ManagementWindow.xaml.cs
+++ Library_App/Windows/ManagementWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using Library_App.Data;
 using Library_App.Models;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Library_App.Windows
 {
@@ -26,26 +18,30 @@ namespace Library_App.Windows
             _context = new LibraryContext();
             _management = new Management();
         }
-
         private void BtnAdd_Click(object sender, RoutedEventArgs e)
         {
             Double Number;
-            if (Double.TryParse(TxtName.Text, out Number) && Double.TryParse(TxtSurname.Text, out Number))
+            if (Double.TryParse(TxtName.Text, out Number) || Double.TryParse(TxtSurname.Text, out Number))
             {
                 MessageBox.Show("Ad ve Soyad reqem ola bilmez");
                 return;
             }
-
-            if (string.IsNullOrEmpty(TxtName.Text) && string.IsNullOrEmpty(TxtSurname.Text)
-    && string.IsNullOrEmpty(TxtUsername.Text) && string.IsNullOrEmpty(TxtPassword.Text)&& string.IsNullOrEmpty((string)CbxPosition.SelectedItem))
+            if (string.IsNullOrWhiteSpace(TxtName.Text) || string.IsNullOrWhiteSpace(TxtSurname.Text)
+             || string.IsNullOrWhiteSpace(TxtUsername.Text) || string.IsNullOrWhiteSpace(TxtPassword.Text))
             {
-                MessageBox.Show("bos ola bilmez");
+                MessageBox.Show("xanalar boş qala bilmez");
                 return;
             }
-            //if ()
-            //{
-
-            //}
+            if (CbxPosition.SelectedIndex == -1)
+            {
+                MessageBox.Show($"Position seçilmemişdir");
+                return;
+            }
+            if (_context.Managements.Any(r => r.UserName == TxtUsername.Text)) 
+            {
+                MessageBox.Show($"Bu Username mövcüddür");
+                return;
+            }
             _management = new Management
             {
                 Name = TxtName.Text,
@@ -55,17 +51,11 @@ namespace Library_App.Windows
                 Position = (Positions)CbxPosition.SelectedItem
             };
             _context.Managements.Add(_management);
-
             _context.SaveChanges();
             MessageBox.Show($"{TxtUsername.Text} adli istifadəçi {(Positions)CbxPosition.SelectedItem} statusu ilə əlavə edildi");
             Reset();
             FillManagements();
-        }
-
-        private void FillManagements()
-        {
-            DgPerson.ItemsSource = _context.Managements.ToList();
-        }
+        }       
         private void DgPerson_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (DgPerson.SelectedItem == null) return;
@@ -77,8 +67,9 @@ namespace Library_App.Windows
             TxtUsername.Text= _selectedManagement.UserName;
             TxtPassword.Text = _selectedManagement.Parol;
             CbxPosition.SelectedItem = _selectedManagement.Position;
+            BtnUpdate.Visibility = Visibility.Visible;
+            BtnDelete.Visibility = Visibility.Visible;
         }
-
         private void BtnUpdate_Click(object sender, RoutedEventArgs e)
         {
             _selectedManagement.Name = TxtName.Text;
@@ -86,13 +77,28 @@ namespace Library_App.Windows
             _selectedManagement.UserName = TxtUsername.Text;
             _selectedManagement.Parol = TxtPassword.Text;
             _selectedManagement.Position = (Positions)CbxPosition.SelectedItem;
-
             _context.SaveChanges();
             Reset();
             MessageBox.Show("İdarəçi dəyişiklikləri yeniləndi");
             FillManagements();
         }
-
+        private void BtnRead_Click(object sender, RoutedEventArgs e)
+        {
+            FillManagements();
+            Reset();
+        }
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult r = MessageBox.Show("Silməyə əminsiniz?", _selectedManagement.ToString(), MessageBoxButton.OKCancel);
+            if (r == MessageBoxResult.OK)
+            {
+                _context.Managements.Remove(_selectedManagement);
+                _context.SaveChanges();
+                Reset();
+                MessageBox.Show($"{_selectedManagement.UserName } adlı İdarəçi silindi") ;
+                FillManagements();
+            }
+        }
         private void Reset()
         {
             TxtName.Clear();
@@ -100,26 +106,13 @@ namespace Library_App.Windows
             TxtUsername.Clear();
             TxtPassword.Clear();
             CbxPosition.SelectedItem = null;
+            BtnUpdate.Visibility = Visibility.Hidden;
+            BtnDelete.Visibility = Visibility.Hidden;
+            BtnRead.Visibility = Visibility.Hidden;
         }
-
-        private void BtnRead_Click(object sender, RoutedEventArgs e)
+        private void FillManagements()
         {
-            FillManagements();
-        }
-        private void BtnDelete_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBoxResult r = MessageBox.Show("Silməyə əminsiniz?", _selectedManagement.ToString(), MessageBoxButton.OKCancel);
-
-            if (r == MessageBoxResult.OK)
-            {
-                _context.Managements.Remove(_selectedManagement);
-                _context.SaveChanges();
-
-                Reset();
-
-                MessageBox.Show($"{_selectedManagement.UserName } adlı İdarəçi silindi") ;
-                FillManagements();
-            }
+            DgPerson.ItemsSource = _context.Managements.ToList();
         }
     }
 }
